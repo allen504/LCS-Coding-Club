c27df46d9ea773319f419c5c1f1e164256bc3555
Joseph Allen <jallen17@tulane.edu>
2020-03-02 12:45:50 -0600

    Create javascript.js

---
+++ programming/javascript.js
@@ -0,0 +1,9 @@
+//JavaScript IDE's: https://playcode.io/, https://jsfiddle.net/, https://js.do/
+
+//TO DO:
+
+//use console.log() to print something to the console
+
+//create a variable and store a number in it
+
+
