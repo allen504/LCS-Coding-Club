af6b01433ddda7d4469b312802264071f62e711a
Joseph Allen <jallen17@tulane.edu>
2020-03-02 12:54:04 -0600

    Create python.py

---
+++ programming/python.py
@@ -0,0 +1,7 @@
+#python IDE's: https://repl.it/languages/python3, https://www.onlinegdb.com/online_python_compiler, https://ideone.com/
+
+#TO DO:
+
+#print something to the console with print()
+
+#create a variable and store a number in it
