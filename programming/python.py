e8416e2fd7c70637278e117efbd90e99434b9bd5
Joseph Allen <jallen17@tulane.edu>
2020-03-09 07:03:09 -0500

    some Python syntax
    variable types, print() function, if-else statement, for loop

--- programming/python.py
+++ programming/python.py
@@ -1,7 +1,25 @@
-#python IDE's: https://repl.it/languages/python3, https://www.onlinegdb.com/online_python_compiler, https://ideone.com/
+#Python IDE's: https://repl.it/languages/python3, https://www.onlinegdb.com/online_python_compiler, https://ideone.com/
 
-#TO DO:
+#Declare variables in Python
+age = 24
+time = 9.57
+character = "A"
+name = "Joseph"
+true_or_false = True
 
-#print something to the console with print()
+#print with print() function
+print("Hi, my name is " + name + ". I'm " + str(age) + " years old. The first letter of my last name is " + character + ", and I can run the 100-yard dash in " + str(time) + " seconds.")
 
-#create a variable and store a number in it
+#if-else statement
+if age > 12:
+  print("You can watch Avengers!")
+else:
+  print("You're too young to watch Avengers!")
+  
+#for loop
+for i in range(10):
+  print ("hello"+str(i))
+
+#while loop?
+
+#function?
