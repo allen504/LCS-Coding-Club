7f877b80946927ed777c96c3cbf34191409d8fbc
Joseph Allen <jallen17@tulane.edu>
2020-03-09 14:44:03 -0500

    some C# syntax

---
+++ programming/csharp.cs
@@ -0,0 +1,38 @@
+// C# IDE: https://www.onlinegdb.com/
+
+//variables in C#
+
+//integer
+int age = 24;
+
+//double
+double time = 9.57;
+
+//character (single quotes only)
+char letter = 'A';
+
+//string
+string name = "Joseph";
+
+//boolean
+bool p = true;
+
+//print with Console.WriteLine()
+Console.WriteLine("Hi, my name is " + name + ". I'm " + age.ToString() + " years old. The first letter of my last name is " + letter + ", and I can run the 100-yard dash in " + time.ToString() + " seconds.");
+
+//if-else statement
+if (age > 12){
+  Console.WriteLine("You can watch Avengers!");
+}
+else{
+  Console.WriteLine("You're too young to watch Avengers!");
+}
+
+//for loop
+for (int i = 0; i < 10; i++){
+  Console.WriteLine("hello"+i);
+}
+
+//while loop?
+
+//function?
