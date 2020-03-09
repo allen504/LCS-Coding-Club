af0d4e2a772553641c222e3135139810646847e5
Joseph Allen <jallen17@tulane.edu>
2020-03-09 07:25:41 -0500

    some JavaScript syntax
    data types, print() function, if-else statement, for loop

--- programming/javascript.js
+++ programming/javascript.js
@@ -1,9 +1,29 @@
 //JavaScript IDE's: https://playcode.io/, https://jsfiddle.net/, https://js.do/
 
-//TO DO:
 
-//use console.log() to print something to the console
+//data types in JavaScript
+var age = 24;
+var time = 9.57;
+var letter = 'A';
+var name = "Joseph";
+var p = true;
 
-//create a variable and store a number in it
+//print something with print() function
+print("Hi, my name is " + name + ". I'm " + age + " years old. The first letter of my last name is " + character + ", and I can run the 100-yard dash in " + time + " seconds.");
 
+//if-else statement
+if (age > 12){
+  print("You can watch Avengers!");
+}
+else{
+  print("You're too young to watch Avengers!");
+}
 
+//for loop
+for (var i = 0; i < 10; i++){
+  print("hello" + i)
+}
+
+//while loop?
+
+//function?
